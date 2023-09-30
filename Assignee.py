6aa6aac8248e0d8eb31e20e4d60bdd9e1652a205
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-09-30 14:01:40 +0300

    Add Task and Assignee

--- Assignee.py
+++ Assignee.py
@@ -1,3 +1,7 @@
+from datetime import time
+
 class Assignee():
-    def __init__(self, id: int) -> None:
+    def __init__(self, id: int, stack: set[str], schedule: list[list[list[time]]]) -> None:
         self.id = id
+        self.stack
+        self.schedule = schedule
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from GeneticAlgorithm import GeneticAlgorithm
 from datetime import timedelta, datetime
 from Assignee import Assignee
+from Task import Task
+from Assignee import Assignee
 
 durations = [timedelta(seconds=8), timedelta(seconds=1), timedelta(seconds=1), timedelta(seconds=5)]
 children = [None, 2, 3, None]
@@ -8,3 +10,7 @@ parents = [None, None, 1, 2]
 assignees = [Assignee(0), Assignee(1), Assignee(2)]
 ga = GeneticAlgorithm([None]*4, durations, children, parents, assignees)
 print(ga.get_best_solution().fitness_score)
+
+
+def run_algorithm(tasks: list[Task], team_memebers: list[Assignee]) -> list[Task]:
+    return []
